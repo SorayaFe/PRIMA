362dee68b45864d70e835377877b41d040c4a366
Soraya Ferdani <soraya.ferdani@googlemail.com>
2022-03-22 17:19:47 +0100

    pacman

--- PacMan/Script/Source/Main.ts
+++ PacMan/Script/Source/Main.ts
@@ -3,17 +3,27 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let pacman: ƒ.Node;
+  let speed: ƒ.Vector3 = new ƒ.Vector3(1 / 60, 1 / 60, 0);
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
+    const graph: ƒ.Node = viewport.getBranch();
+
+    pacman = graph.getChildrenByName("Pacman")[0];
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+
+    pacman.mtxLocal.translate(speed);
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
