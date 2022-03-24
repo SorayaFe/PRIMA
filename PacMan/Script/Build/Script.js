122c3b7cc9689c11c1c4b5b018b5898b73b0a152
Soraya Ferdani <soraya.ferdani@googlemail.com>
2022-03-24 15:26:09 +0100

    move pacman

--- PacMan/Script/Build/Script.js
+++ PacMan/Script/Build/Script.js
@@ -42,38 +42,47 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let pacman;
-    let movement = new ƒ.Vector3(1 / 80, 0, 0);
+    let movement = new ƒ.Vector3(1 / 60, 0, 0);
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
         const graph = viewport.getBranch();
         pacman = graph.getChildrenByName("Pacman")[0];
-        document.addEventListener("keydown", movePacman);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
+        movePacman();
         pacman.mtxLocal.translate(movement);
         viewport.draw();
         ƒ.AudioManager.default.update();
     }
-    function movePacman(_event) {
-        switch (_event.code) {
-            case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
-                movement = new ƒ.Vector3(1 / 80, 0, 0);
-                break;
-            case ƒ.KEYBOARD_CODE.ARROW_DOWN:
-                movement = new ƒ.Vector3(0, -1 / 80, 0);
-                break;
-            case ƒ.KEYBOARD_CODE.ARROW_LEFT:
-                movement = new ƒ.Vector3(-1 / 80, 0, 0);
-                break;
-            case ƒ.KEYBOARD_CODE.ARROW_UP:
-                movement = new ƒ.Vector3(0, 1 / 80, 0);
-                break;
-            default:
-                break;
+    function movePacman() {
+        if (ƒ.Keyboard.isPressedOne([
+            ƒ.KEYBOARD_CODE.ARROW_RIGHT,
+            ƒ.KEYBOARD_CODE.D,
+        ]) &&
+            (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
+            movement.set(1 / 60, 0, 0);
+        }
+        if (ƒ.Keyboard.isPressedOne([
+            ƒ.KEYBOARD_CODE.ARROW_DOWN,
+            ƒ.KEYBOARD_CODE.S,
+        ]) &&
+            (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+            movement.set(0, -1 / 60, 0);
+        }
+        if (ƒ.Keyboard.isPressedOne([
+            ƒ.KEYBOARD_CODE.ARROW_LEFT,
+            ƒ.KEYBOARD_CODE.A,
+        ]) &&
+            (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
+            movement.set(-1 / 60, 0, 0);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) &&
+            (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+            movement.set(0, 1 / 60, 0);
         }
     }
 })(Script || (Script = {}));
--- PacMan/Script/Source/Main.ts
+++ PacMan/Script/Source/Main.ts
@@ -4,7 +4,7 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
-  let movement: ƒ.Vector3 = new ƒ.Vector3(1 / 80, 0, 0);
+  let movement: ƒ.Vector3 = new ƒ.Vector3(1 / 60, 0, 0);
 
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
@@ -15,8 +15,6 @@ namespace Script {
 
     pacman = graph.getChildrenByName("Pacman")[0];
 
-    document.addEventListener("keydown", movePacman);
-
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
@@ -24,28 +22,50 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
 
+    movePacman();
+
     pacman.mtxLocal.translate(movement);
 
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
 
-  function movePacman(_event: KeyboardEvent) {
-    switch (_event.code) {
-      case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
-        movement = new ƒ.Vector3(1 / 80, 0, 0);
-        break;
-      case ƒ.KEYBOARD_CODE.ARROW_DOWN:
-        movement = new ƒ.Vector3(0, -1 / 80, 0);
-        break;
-      case ƒ.KEYBOARD_CODE.ARROW_LEFT:
-        movement = new ƒ.Vector3(-1 / 80, 0, 0);
-        break;
-      case ƒ.KEYBOARD_CODE.ARROW_UP:
-        movement = new ƒ.Vector3(0, 1 / 80, 0);
-        break;
-      default:
-        break;
+  function movePacman() {
+    if (
+      ƒ.Keyboard.isPressedOne([
+        ƒ.KEYBOARD_CODE.ARROW_RIGHT,
+        ƒ.KEYBOARD_CODE.D,
+      ]) &&
+      (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05
+    ) {
+      movement.set(1 / 60, 0, 0);
+    }
+
+    if (
+      ƒ.Keyboard.isPressedOne([
+        ƒ.KEYBOARD_CODE.ARROW_DOWN,
+        ƒ.KEYBOARD_CODE.S,
+      ]) &&
+      (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05
+    ) {
+      movement.set(0, -1 / 60, 0);
+    }
+
+    if (
+      ƒ.Keyboard.isPressedOne([
+        ƒ.KEYBOARD_CODE.ARROW_LEFT,
+        ƒ.KEYBOARD_CODE.A,
+      ]) &&
+      (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05
+    ) {
+      movement.set(-1 / 60, 0, 0);
+    }
+
+    if (
+      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) &&
+      (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05
+    ) {
+      movement.set(0, 1 / 60, 0);
     }
   }
 }
