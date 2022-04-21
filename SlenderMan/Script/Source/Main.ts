7bd9a5b13dd18d58aa3ceff554b3af5da9752b51
Soraya Ferdani <soraya.ferdani@googlemail.com>
2022-04-21 15:35:37 +0200

    slenderman camera and movement

--- SlenderMan/Script/Source/Main.ts
+++ SlenderMan/Script/Source/Main.ts
@@ -3,18 +3,59 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let avatar: ƒ.Node;
+  let camera: ƒ.ComponentCamera;
+
+  const speedRotY: number = -0.1;
+  const speedRotX: number = 0.2;
+  let rotationX: number = 0;
+  let cntrWalk: ƒ.Control = new ƒ.Control("cntrWalk", 6, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  cntrWalk.setDelay(350);
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    avatar = viewport.getBranch().getChildrenByName("Avatar")[0];
+    camera = avatar.getChild(0).getComponent(ƒ.ComponentCamera);
+    viewport.camera = camera;
+
+    viewport.getCanvas().addEventListener("pointermove", hndPointerMove);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    controlWalk();
+
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  function hndPointerMove(_event: PointerEvent): void {
+    avatar.mtxLocal.rotateY(_event.movementX * speedRotY);
+
+    rotationX += _event.movementY * speedRotX;
+    rotationX = Math.min(60, Math.max(-60, rotationX));
+    camera.mtxPivot.rotation = ƒ.Vector3.X(rotationX);
+  }
+
+  function controlWalk(): void {
+    let input: number = ƒ.Keyboard.mapToTrit(
+      [ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP],
+      [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]
+    );
+
+    cntrWalk.setInput(input);
+
+    let input2: number = ƒ.Keyboard.mapToTrit(
+      [ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT],
+      [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]
+    );
+
+    avatar.mtxLocal.translateZ((cntrWalk.getOutput() * ƒ.Loop.timeFrameGame) / 1000);
+    avatar.mtxLocal.translateX((6 * input2 * ƒ.Loop.timeFrameGame) / 1000);
   }
 }
